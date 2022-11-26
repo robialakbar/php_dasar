64c8dbed01b4f5973a1355e393927523c7a463b7
robialakbar <robialakbar@gmail.com>
2022-11-26 21:34:51 +0700

    Belajar PHP Video ke 4

---
+++ Pertemuan_2/index_1.php
@@ -0,0 +1,6 @@
+<?php
+echo "Saya robi al akbar";
+echo "\r\n";
+print" Saya Seorang Programmer ! ";
+var_dump("Hallo");
+?>
---
+++ Pertemuan_2/index_5.php
@@ -0,0 +1,10 @@
+<?php
+
+$nama_depan = "robi";
+$nama_belakang = "al akbar";
+$umur = "29";
+$agama = "Islam";
+
+echo $nama_depan . " " . $nama_belakang . " " . $umur . " " .  $agama;
+
+?>
