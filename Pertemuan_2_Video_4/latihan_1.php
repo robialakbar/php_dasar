10d944a5813daedbcdb754b1de680a05a2c05cb5
robialakbar <robialakbar@gmail.com>
2023-01-18 23:32:26 +0700

    Perulangan While

--- Pertemuan_2_Video_4/latihan_1.php
+++ Pertemuan_2_Video_4/latihan_1.php
@@ -4,7 +4,13 @@
   for ($i = 0; $i < 5; $i++) {
     echo "Hello World <br>" ;
   }
+  
+  $o=0;
   //while
+  while( $o<5) {
+echo "Hello World! <br>";
+$o++;
+  }
   //do while
   //foreach : pengulangan khusus array
 
